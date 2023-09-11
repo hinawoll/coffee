0f0effb8beec68f7d1c23b27df97c6fd8c691211
hina <143842175+hinawoll@users.noreply.github.com>
2023-09-11 15:27:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('header a').click(function(){
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html, body').animate({
+      'scrollTop': position 
+    }, 600);
+  });
+    
+});
+  
